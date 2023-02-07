b9146428b336ba5ea8cadf2e29aff09b8d942566
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-02-07 16:48:01 -0700

    Adjust event handler logic
    Made some changes to the event handler logic.
    
    Originally I wanted clicking the button to return a value,
    but I learned that event handlers don't return anything.
    Determined (again) to run playRound() inside playOnClick().
    
    Replaced the console logging of the round's results with inserting
    into the HTML.
    
    Deleted game() as it will need extensive reworking in order to
    do what I want it to do.

--- rps.js
+++ rps.js
@@ -15,75 +15,55 @@ function getComputerChoice() {
     }
 }
 
+
 const results = document.querySelector('#results');
+const score = document.querySelector('#score');
 
 //compare one computer generated val with one user-provided val
 function playRound (playerSelection, computerSelection) {
     console.log(`player chose: ${playerSelection}. Computer chose: ${computerSelection}.`);
 
     if (playerSelection === computerSelection) {
-        console.log('tie! no new points');
         results.textContent = 'tie! No new points.';
         return 'tie';
     } else if (computerSelection === 'rock') {
         if (playerSelection === 'paper') {
-            console.log('You win! Paper beats rock.');
             results.textContent = 'You win! Paper beats rock.';
             return 'win';
         } else if (playerSelection === 'scissors') {
-            console.log('You lose! Rock beats Scissors.')
             results.textContent = 'You lose! Rock beats scissors.';
             return 'lose';
         }
     } else if (computerSelection === 'paper') {
         if (playerSelection === 'rock') {
-            console.log('You lose! Paper beats rock.');
+            results.textContent = 'You lose! Paper beats rock.'
             return 'lose';
         } else if (playerSelection === 'scissors') {
-            console.log('You win! Scissors beat paper.');
             results.textContent = 'You win! Scissors beat paper.';
             return 'win';
         }
     } else if (computerSelection === 'scissors') {
         if (playerSelection === 'rock')  {
-            console.log('You win! Rock beats scissors.');
             results.textContent = 'You win! Rock beats scissors.';
             return 'win';
         } else if (playerSelection === 'paper') {
-            console.log('You lose! Scissors beat paper.');
             results.textContent = 'You lose! Scissors beat paper.';
             return 'lose';
         }
     }
-    if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
-        console.log('please enter a valid input or check your spelling.');
-    }
 }
 
 //put event listeners on the buttons.
-//event listener should run the playround() function when clicked.
-const btns = document.querySelectorAll("button");
-btns.forEach((button) => {
-    button.addEventListener('click', () => {
-        playRound(button.className, getComputerChoice());
+//run playRound when clicked to compare button value with computer choice
+
+function playOnClick() {
+    const btns = document.querySelectorAll("button");
+    btns.forEach((button) => {
+        button.addEventListener('click', () => {
+            playRound(button.className, getComputerChoice());
+        });
     });
-});
-
-
-//playRound repeated 5 times. Tally the score.
-/*function game (output) {
-    let counter = 0;
-    for (let i = 0; i < 5; i++) {
-        output = playRound(getPlayerChoice(), getComputerChoice());
-        if (output === 'win') {
-            counter++;
-        } else if (output === 'lose') {
-            counter--;
-        }
-    }
-    console.log(counter);
-    //return counter;
 }
 
-game();*/
+//playRound repeated until player or computer gets 5 points.
 
