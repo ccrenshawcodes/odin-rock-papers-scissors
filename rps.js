e8cca4d987ea8a64350440b86c361379d3b5dd4b
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-02-09 12:46:06 -0700

    Add a keepScore() function
    Added a keepScore function that increments counters and displays a
    'winner' message when one user reaches 5 points.
    
    This resolves the scope issues I was having before and the difficulties
    I was having with keeping score and ending the game.

--- rps.js
+++ rps.js
@@ -1,3 +1,11 @@
+//global variables list
+const resultsMsg = document.querySelector('#results');
+const score = document.querySelector('#playerScore');
+const compScore = document.querySelector('#compScore');
+const winnerMsg = document.querySelector('#winner');
+let playerCounter = 0; 
+let compCounter = 0;
+
 //get a random number.
 //assign number to string value.
 function getComputerChoice() {
@@ -15,68 +23,67 @@ function getComputerChoice() {
     }
 }
 
-//global variables list
-const results = document.querySelector('#results');
-const score = document.querySelector('#playerScore');
-const compScore = document.querySelector('#compScore');
-const winner = document.querySelector('#winner');
-let playerCounter = 0; 
-let compCounter = 0;
+//increment the score.
+//update the displays.
+//stop the game at 5 points & display winner message
+function keepScore(result) {
+  if (result === 'win') {
+    playerCounter++;
+    score.textContent = `Your score: ${playerCounter}`; 
+    compScore.textContent = `Computer score: ${compCounter}`;
+    
+  } else if (result === 'lose') {
+    compCounter++;
+    score.textContent = `Your score: ${playerCounter}`; 
+    compScore.textContent = `Computer score: ${compCounter}`;
+    
+  }
+  
+  if (playerCounter >= 5 || compCounter >= 5) {
+    if (playerCounter >= 5) {
+        winnerMsg.textContent = 'You win!';
+    } else if (compCounter >= 5) {
+        winnerMsg.textContent = 'wow...you lost to a computer';
+    }
+  }
+  
+}
 
-
-//compare one user generated val with the user inputted val
-//perform a specific function on win or lose
-//end the game after someone reaches 5 points.
+//compare two values (a user selection and a computer selection)
+//keepScore() with each round & update display
 const playRound = (playerSelection, computerSelection) => {
-    console.log(`player chose: ${playerSelection}. Computer chose: ${computerSelection}.`);
 
-    function playerLose() {
-        compCounter++;
-        score.textContent = `Your score: ${playerCounter}`; 
-        compScore.textContent = `Computer score: ${compCounter}`;
-        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-
-    function playerWin() {
-        playerCounter++;
-        score.textContent = `Your score: ${playerCounter}`; 
-        compScore.textContent = `Computer score: ${compCounter}`;
-        results.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
-    }
-
-    if (playerCounter < 5 && compCounter < 5) {
-        if (playerSelection === computerSelection) {
-            results.textContent = 'tie! No new points.';
-        } else if (playerSelection === 'rock') {
-            if (computerSelection === 'paper') {
-                playerLose();
-            } else if (computerSelection === 'scissors') {
-                playerWin();
-            }
-        } else if (playerSelection === 'paper') {
-            if (computerSelection === 'scissors') {
-                playerLose();
-            } else if (computerSelection === 'rock') {
-                playerWin();
-            }
-        } else if (playerSelection === 'scissors') {
-            if (computerSelection === 'rock') {
-                playerLose();
-            } else if (computerSelection === 'paper') {
-                playerWin();
-            }
+    if (playerSelection === computerSelection) {
+        resultsMsg.textContent = 'tie! No new points.';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            keepScore('lose');
+            resultsMsg.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        } else if (computerSelection === 'scissors') {
+            keepScore('win');
+            resultsMsg.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
         }
-        } else if (playerCounter >= 5 || compCounter >= 5) {
-            if (playerCounter >= 5) {
-                winner.textContent = 'You win!';
-            } else if (compCounter >= 5) {
-                winner.textContent = 'wow...you lost to a computer';
-            }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            keepScore('lose');
+            resultsMsg.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        } else if (computerSelection === 'rock') {
+            keepScore('win');
+            resultsMsg.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            keepScore('lose');
+            resultsMsg.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        } else if (computerSelection === 'paper') {
+            keepScore('win');
+            resultsMsg.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+        }
     }
 }
 
 //put event listeners on the buttons.
-//run playRound when clicked to compare button value with computer choice
+//run playRound() when clicked to compare button value with computer choice
 function playOnClick() {
     const btns = document.querySelectorAll("button");
     btns.forEach((button) => {
